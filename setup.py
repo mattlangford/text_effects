892d5b5cd6ceae48c879797234fe4d8260406ca2
Matthew Langford <matthew.langford95@gmail.com>
2017-02-10 14:32:57 -0500

    PYTHON: Make it installable

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='text_effects',
+      version='1.0',
+      description='Add some spice to your text in python',
+      author='Matthew Langford',
+      packages=['text_effects'])
